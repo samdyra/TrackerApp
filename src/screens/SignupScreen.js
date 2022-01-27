16c4b4827059c7d80f7cf6e3a1eec764375f5c1d
samdyra <197413vr@gmail.com>
2022-01-28 01:10:25 +0700

    update UI

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    marginBottom: 350,
+    marginBottom: 300,
   },
   errorMessage: {
     fontSize: 16,
